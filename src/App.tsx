e21ecbd843a9d0e9ee67d69dea175281b6e4ea68
Nikko <nikkosolon24@gmail.com>
2020-05-31 20:25:12 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
 
 function App() {
+  // const renderTile = tile => {
+  //   const {frontImg, backImg, revealed} = tile;
+  //   const img = revealed ? frontImg : backImg;
+  //   return(
+  //     <div className="Tile">
+  //       <img src={img} alt='' />
+  //     </div>
+  //   )
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="text-lg text-center text-black bg-blue-200">Mahjong Online!</div>
+      <button className="item-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Play!
+      </button>
     </div>
   );
 }
