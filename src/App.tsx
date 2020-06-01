3a50631174fe23b731803c4cadd33138132c7303
Nikko Solon <nikkosolon24@gmail.com>
2020-05-31 22:57:49 -0700

    Updated Main app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import logo from './M_Dot1.png'
 import './css/main.css';
+import './App.css'
 
 function App() {
   // const renderTile = tile => {
@@ -12,11 +14,18 @@ function App() {
   //   )
   // }
   return (
-    <div className="background">
-      <div className="text-lg text-center text-black bg-blue-200">Mahjong Online!</div>
-      <button className="item-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Play!
-      </button>
+    <div className="App">
+      <header className='App-header'>
+        <h1 className="font-mono text-6xl text-white py-10">Mahjong Online!</h1>
+        <div className='tile shadow-md'>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className='py-10'>
+          <button className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Play!
+          </button>
+        </div>
+      </header>
     </div>
   );
 }
