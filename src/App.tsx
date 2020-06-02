a8222ec3c161bae1f092f5785b505e3874363767
Nikko Solon <nikkosolon24@gmail.com>
2020-06-01 17:06:34 -0700

    Updated App.tsx to use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import logo from './M_Dot1.png'
-import './css/main.css';
-import './css/App.css'
+import Home from './pages/Home';
+import Table from './pages/Table';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  // const renderTile = tile => {
-  //   const {frontImg, backImg, revealed} = tile;
-  //   const img = revealed ? frontImg : backImg;
-  //   return(
-  //     <div className="Tile">
-  //       <img src={img} alt='' />
-  //     </div>
-  //   )
-  // }
   return (
-    <div className="App">
-      <header className='App-header'>
-        <h1 className="font-mono text-6xl text-white py-10">Mahjong Online!</h1>
-        <div className='tile shadow-md'>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <div className='py-10'>
-          <button className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Play!
-          </button>
-        </div>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path='/play'>
+          <Table />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
