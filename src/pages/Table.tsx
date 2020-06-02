18c370032f7db9b3f26f2db6c4936cfeb390b0d8
Nikko Solon <nikkosolon24@gmail.com>
2020-06-01 17:07:05 -0700

    Created Table.tsx

---
+++ src/pages/Table.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css';
+
+function Table(){
+    return(
+        <div className="App">
+            <header className='App-header'></header>
+        </div>
+    );
+}
+
+export default Table; 
