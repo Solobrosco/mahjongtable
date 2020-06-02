59001a4e0130a55b2aa41a48c2ac2425f64ae865
Nikko Solon <nikkosolon24@gmail.com>
2020-06-01 17:06:48 -0700

    Created Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from '../M_Dot1.png';
+import '../css/main.css';
+import '../css/App.css';
+import {Link} from 'react-router-dom'; 
+
+import Table from './Table';
+
+const play = () => {
+    console.log('clicked');
+    return(
+        <div>
+            <Link to="/play">
+                <Table />
+            </Link>
+        </div>
+    );
+}
+function Home() {
+  // const renderTile = tile => {
+  //   const {frontImg, backImg, revealed} = tile;
+  //   const img = revealed ? frontImg : backImg;
+  //   return(
+  //     <div className="Tile">
+  //       <img src={img} alt='' />
+  //     </div>
+  //   )
+  // }
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h1 className="font-mono text-6xl text-white py-10">Mahjong Online!</h1>
+        <div className='tile shadow-md'>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className='py-10'>
+            <Link to={'/play'}>
+                <button className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={play}>
+                    Play!
+                </button>
+            </Link>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
