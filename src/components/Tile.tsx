c971251c7c2077dd652575c139473609a294c4ec
Nikko <nikkosolon24@gmail.com>
2020-06-04 22:48:48 -0700

    Updatred Tile.tsx for sprite support

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Dot from '../assets/M_Dot1.png';
+import { connect } from 'react-redux';
 import '../css/App.css';
+import tileSprite from '../assets/mahjong_tiles_spritesheet.png';
 
 const Tile = () => {
     return (
-        <div>
-            <div className='tile'>
-                <img src={Dot} alt='Dot1'></img>
-            </div>
-        </div>
+        <div 
+            style={{
+                position: 'relative',
+                backgroundImage: `url('${tileSprite}')`,
+                width: '256px',
+                height: '246px'
+            }}
+        />
+        // <div>
+        //     <div className='tile'>
+        //         <img src={Dot} alt='Dot1'></img>
+        //     </div>
+        // </div>
     );
 }
 
-export default Tile;
+export default connect()(Tile);
