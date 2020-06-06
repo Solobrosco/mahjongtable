046323c28a99bbd711414b35089d52c054d520f1
Nikko <nikkosolon24@gmail.com>
2020-06-05 22:26:56 -0700

    Created revealedHand.tsx

---
+++ src/components/revealedHand.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tile from './Tile';
+
+const revealedHand = () => {
+    return(
+        <div>
+            <Tile />
+            <Tile />
+            <Tile />
+        </div>
+    );
+}
+
+export default revealedHand;
