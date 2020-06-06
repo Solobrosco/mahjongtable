5a7087786675adbb645a63b544792c8e3787afaa
Nikko <nikkosolon24@gmail.com>
2020-06-06 14:41:25 -0700

    Created Calls.tsx

---
+++ src/features/player/functions/Calls.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calls = () => {
+    return(
+        <div className='bg-color-500 bg-blue-900'>
+            I am buttons
+        </div>
+    );
+}
+
+export default Calls;
