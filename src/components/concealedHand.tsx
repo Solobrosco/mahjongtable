857066b16c88129bb8d26da003a344a4ae293411
Nikko <nikkosolon24@gmail.com>
2020-06-05 22:27:12 -0700

    Created concealedHand.tsx

---
+++ src/components/concealedHand.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tile from './Tile';
+
+const concealedHand = () => {
+    return(
+        <div>
+            <Tile />
+            <Tile />
+            <Tile />
+        </div>
+    );
+}
+
+export default concealedHand;
