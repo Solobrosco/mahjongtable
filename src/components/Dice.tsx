0d143a7513eb6c7f1fb9e4572ab27ec106b4b109
Nikko <nikkosolon24@gmail.com>
2020-06-05 22:27:45 -0700

    Created Dice.tsx

---
+++ src/components/Dice.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// A cube with sides numbers 1-6, each representing a side accordingly, is thrown randomly into space landing on number.
+
+// function random(){
+//     return();
+// }
+const Die = () => {
+    return(
+        // <div>
+        //     <ReactDice size={6} />
+        // </div>
+        <div></div>
+    );
+}
+
+export default Die;
