a918ee675f81b04f999f3f700252e8a70499e7f1
Nikko <nikkosolon24@gmail.com>
2020-06-06 14:38:36 -0700

    Renamed revealedHand.tsx to Hand.tsx

---
+++ src/features/Hand.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hand = () => {
+    return(
+        <div className='bg-blue-200'>
+            PlayerHand East
+        </div>
+    );
+}
+
+export default Hand;
