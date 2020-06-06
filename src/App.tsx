1118c90230c405aa2010803b6f4ec3c99e866050
Nikko <nikkosolon24@gmail.com>
2020-06-06 14:40:07 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
-import Table from './pages/Table';
+import Play from './pages/Play';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import { connect } from 'react-redux'
 
 function App() {
   return (
@@ -15,11 +16,11 @@ function App() {
           <Home />
         </Route>
         <Route exact path='/play'>
-          <Table />
+          <Play />
         </Route>
       </Switch>
     </Router>
   );
 }
 
-export default App;
+export default connect()(App);  
