e1163a942cf14efee11e1edb96bce5131e1c1942
Nikko <nikkosolon24@gmail.com>
2020-06-06 14:40:59 -0700

    Described Player.tsx

--- src/features/player/Player.tsx
+++ src/features/player/Player.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Player can call shang, pong, and kong
+// Player can throwaway tile
+// Player can reorder their tiles or the game can order the tiles automatically
+// rolls dice
 const Player = () => {
     return(
         <div>
