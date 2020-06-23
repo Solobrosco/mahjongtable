9e98d3089580e0563b31281e701b8b083a178f25
Nikko Solon <nikkosolon24@gmail.com>
2020-06-23 16:29:56 -0700

    Edited Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,13 +29,13 @@ function Home() {
   return (
     <div className="App">
       <header className='App-header'>
-        <h1 className="font-mono text-6xl text-white py-10">Mahjong Online!</h1>
-        <div className='Tile-logo tile'>
+        <h1 className="scale font-mono text-6xl text-white py-10">Mahjong Online!</h1>
+        <div className='scale tile'>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className='py-10'>
             <Link to={'/play'}>
-                <button className="shadow-lg font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={play}>
+                <button className="scale shadow-lg font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={play}>
                     Play!
                 </button>
             </Link>
