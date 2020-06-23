7f2b3ff8a2b89116fc9000d6797797afec697cce
Nikko <nikkosolon24@gmail.com>
2020-06-22 21:07:30 -0700

    Edited Dice.tsx

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 
-// A cube with sides numbers 1-6, each representing a side accordingly, is thrown randomly into space landing on number.
-
-// function random(){
-//     return();
-// }
-interface DieProps {
-    numberOfDice: null,
-    rolls: [],
-    rollSum: []
-}
-
-const Dice = (props: DieProps) => {
+const Dice = () => {
+    let [sum, setTot] = React.useState('');
+    let getSum = () => {
+        // tweak this later for displaying the dice
+        let rollDice = () => {
+            var tot : number = Math.floor(Math.random() * 6 ) + 1;
+            tot += Math.floor(Math.random() * 6 ) + 1;
+            return tot;
+        }
+        setTot(rollDice().toString());
+    }
+    // Player(E) rolls dice and dealer is chosen counting counter clockwise S-W-N-E-...
+    // All possible dealers chosen
+    // W-N-E-S-W-N-E-S-W--N--E-
+    // 2-3-4-5-6-7-8-9-10-11-12
+    // after this, dealer rolls again to break the number section of their wall
+    // keep it simple - no need for implementation (yet)
     return(
-        // <div>
-        //     <ReactDice size={6} />
-        // </div>
-    <div>{props}</div>
+        <div>
+            <button onClick={getSum} className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Roll Dice {sum}
+            </button>
+        </div>
     );
 }
 
