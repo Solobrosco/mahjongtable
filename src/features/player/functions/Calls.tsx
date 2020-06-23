05b6776581d0b587f1de4f2cbc41f2a9c6f0eba1
Nikko <nikkosolon24@gmail.com>
2020-06-22 21:05:26 -0700

    Added called functions in Calls.tsx

--- src/features/player/functions/Calls.tsx
+++ src/features/player/functions/Calls.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+let callShang = () => {
+    // check player hand, check if recent player is North, and recent thrown for shang 
+    console.log('Shang!')
+}
+let callPong = () => {
+    // check player hand and recent thrown for pong
+    console.log('Pong!')
+}
+let callKong = () => {
+    // check player hand and recent thrown for kong
+    console.log('Kong!')
+}
+
 const Calls = () => {
     return(
         <div>
-            <button className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={callShang} className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Shang!
             </button>
-            <button className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={callPong} className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Pong!
             </button>
-            <button className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={callKong} className="shadow-md font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Kong!
             </button>
         </div>
