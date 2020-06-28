94beb65c72d1c3a8eb8879d5a936d80c7280e4ce
Nikko Solon <nikkosolon24@gmail.com>
2020-06-27 17:56:27 -0700

    Added Props and div content to Table.tsx

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import Tile from './Tile';
+import Dice from './Dice';
 
-const Table = () => {
+type TableProps = {
+    mahjongPieces?: {},
+    playerList?: Array <string>
+}
+
+const Table = (props: TableProps) => {
     return(
-        <div className='table rounded-md'> 
+        <div className='flex container mx-auto bg-blue-800 p-5'>
+            <div className='thrown'>
+                <Tile />
+            </div>
+            <div className='revealed'>
+                revealed
+            </div>
+            <div className='southRevealed'>
+                south
+            </div>
+            <div className='westRevealed'>
+                west
+            </div>
+            <div className='northRevealed'>
+                north
+            </div>
+            <Dice />
         </div>
     );
 }
