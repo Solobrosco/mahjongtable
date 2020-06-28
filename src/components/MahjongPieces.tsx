10fbaa95b28fbeac6a24333d91d62cfa6fbcc2f3
Nikko Solon <nikkosolon24@gmail.com>
2020-06-27 18:02:15 -0700

    Created MahjongPieces.tsx

---
+++ src/components/MahjongPieces.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// A library of mahjong pieces on the table.
+
+type MahjongPiecesProps = {
+    thrown?: Array<number>,
+}
+
+
+const MahjongPieces = (props: MahjongPiecesProps) => {
+    return (
+        <div>
+            basic fucn
+        </div>
+    );
+}
+
+export default MahjongPieces; 
