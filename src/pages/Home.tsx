40074d47253f81a265d77cf0223fb683536aeec8
Nikko Solon <nikkosolon24@gmail.com>
2020-06-27 17:55:09 -0700

    Home.tsx HouseKeeping

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,29 +3,22 @@ import logo from '../assets/M_Dot1.png';
 import '../css/main.css';
 import '../css/App.css';
 import {Link} from 'react-router-dom'; 
+// import Grid from '@material-ui/core/Grid';
 
+// Other pages
 import Play from './Play';
 
 const play = () => {
-    console.log('clicked');
-    return(
-        <div>
-            <Link to="/play">
-                <Play />
-            </Link>
-        </div>
-    );
+  // console.log('clicked');
+  return(
+    <div>
+      <Link to="/play">
+        <Play />
+      </Link>
+    </div>
+  );
 }
 function Home() {
-  // const renderTile = tile => {
-  //   const {frontImg, backImg, revealed} = tile;
-  //   const img = revealed ? frontImg : backImg;
-  //   return(
-  //     <div className="Tile">
-  //       <img src={img} alt='' />
-  //     </div>
-  //   )
-  // }
   return (
     <div className="App">
       <header className='App-header'>
@@ -34,11 +27,11 @@ function Home() {
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className='py-10'>
-            <Link to={'/play'}>
-                <button className="scale shadow-lg font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={play}>
-                    Play!
-                </button>
-            </Link>
+          <Link to={'/play'}>
+            <button className="scale shadow-lg font-mono text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={play}>
+              Play!
+            </button>
+          </Link>
         </div>
       </header>
     </div>
