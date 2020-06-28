41fe21414a952de1948f5d31416fc21326ee1bf3
Nikko Solon <nikkosolon24@gmail.com>
2020-06-27 17:55:49 -0700

    Implemented Material UI Grid to Play.tsx

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -4,7 +4,9 @@ import PlayersHand from '../components/Hand';
 import Table from '../components/Table';
 import PlayerFunction from '../features/player/functions/Calls';
 // import {useSelector} from 'react-redux';
-import Dice from '../components/Dice';
+
+// Material UI Grid
+import Grid from '@material-ui/core/Grid';
 
 function Play(){
     // const gamestate = useSelector(state=>false);
@@ -12,12 +14,17 @@ function Play(){
     let r : Array<number> = [3,5,7];
     return(
         <div className="App-header">
-            <Dice/>
-            <div className='flex'>
-                <Table />
+            <div className='w-auto flex bg-blue-900 p-5 justify-center items-center'>
+                <Grid >
+                    <Grid>
+                        <Table />
+                        <div className="pt-5">
+                            <PlayersHand hand={h} revealed={r}/>
+                        </div>
+                        <PlayerFunction />
+                    </Grid>
+                </Grid>
             </div>
-            <PlayersHand hand={h} revealed={r}/>
-            <PlayerFunction />
         </div>
     );
 }
